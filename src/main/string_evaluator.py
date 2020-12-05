a523690e8163cdbceed50a1c72a9a9dd04c5feb8
simulationpoint <55375400+simulationpoint@users.noreply.github.com>
2020-12-05 17:15:41 -0500

    v.0.2

--- src/main/string_evaluator.py
+++ src/main/string_evaluator.py
@@ -1,19 +1,35 @@
 # Created by Leon Hunter at 3:57 PM 10/23/2020
 class StringManipulator(object):
     def get_hello_world(self):
-        return None # TODO - Implement solution
+        message = "Welcome to DevOps 2020"
+        return message # return string
 
     def concatenate(self, value_to_be_added_to, value_to_add):
-        return None # TODO - Implement solution
+        string_concat = (str(value_to_be_added_to) + str(value_to_add))
+        return string_concat # returns concatenated string
 
     def substring_inclusive(self, string_to_fetch_from, starting_index, ending_index):
-        return None # TODO - Implement solution
+        inclusive_op=(string_to_fetch_from [starting_index:ending_index + 1])
+        return inclusive_op # inclusive string manipulation
 
     def substring_exclusive(self, string_to_fetch_from, starting_index, ending_index):
-        return None # TODO - Implement solution
+        exclusive_op= (string_to_fetch_from[starting_index + 1:ending_index])
+        return exclusive_op # exclusive string manipulation
 
     def compare(self, first_value, second_value):
-        return None # TODO - Implement solution
+        if [(first_value > second_value)]:
+            text1 = "first string value is greater"
+            return  text1
+            else if [(first_value < second_value)]:
+                text2 = "first string value is less than the second value"
+                return  text2
+            else if [(first_value == second_value)]:
+            text3="the two values are equal"
+            return text3
+        else if [(first_value != second_value)]:
+        text4=" The two sting values are not equal"
+    return text4
+        return None # compare two strings
 
     def get_middle_character(self, string_to_fetch_from):
         return None # TODO - Implement solution
