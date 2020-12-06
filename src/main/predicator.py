b0dbf34a451e9eba4f839e6505a85b4a783c3457
simulationpoint <55375400+simulationpoint@users.noreply.github.com>
2020-12-05 22:59:38 -0500

    Update predicator.py

--- src/main/predicator.py
+++ src/main/predicator.py
@@ -3,25 +3,25 @@
 #
 class Predicator(object):
     def is_greater_than_5(self, some_value):
-        if [some_value >= 5]:
+        if some_value > 5:
             return True
         else:
             return False
 
     def is_greater_than_8(self, some_value):
-        if [some_value >= 8]:
+        if some_value > 8:
             return True
         else:
             return False
 
     def is_less_than_4(self, some_value):
-        if [some_value < 4]:
+        if some_value < 4:
             return True
         else:
             return False
 
     def is_less_than_1(self, some_value):
-        if [some_value < 1]:
+        if some_value < 1:
             return True
         else:
             return False
